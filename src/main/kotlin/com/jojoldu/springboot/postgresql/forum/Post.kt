ad1227848123b2be2d6e8e969e0bbd17a98100b4
jojoldu <jojoldu@gmail.com>
2023-06-20 18:49:59 +0900

    kotlin 플러그인 추가

--- src/main/kotlin/com/jojoldu/springboot/postgresql/forum/Post.kt
+++ src/main/kotlin/com/jojoldu/springboot/postgresql/forum/Post.kt
@@ -3,16 +3,23 @@ package com.jojoldu.springboot.postgresql.forum
 import jakarta.persistence.*
 
 @Entity
-class Post (
+class Post () {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
-
+    
     @Column
     var name: String,
-
+    
     @Column
     var age: Int,
-
-
-)
+    
+    constructor(id: Long, name: String, age: Int) : this() {
+        this.id = id
+        this.name = name
+        this.age = age
+    }
+    
+    
+    
+}
