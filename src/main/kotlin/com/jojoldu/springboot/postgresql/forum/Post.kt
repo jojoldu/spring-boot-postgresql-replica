003caef823379d4678c6dbbd6a1c157fb88757ec
jojoldu <jojoldu@gmail.com>
2023-07-09 23:03:30 +0900

    AI 리팩토링

--- src/main/kotlin/com/jojoldu/springboot/postgresql/forum/Post.kt
+++ src/main/kotlin/com/jojoldu/springboot/postgresql/forum/Post.kt
@@ -3,23 +3,14 @@ package com.jojoldu.springboot.postgresql.forum
 import jakarta.persistence.*
 
 @Entity
-class Post () {
+class Post (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long,
-
-    @Column
-    var name: String = "",
+    var id: Long = 0,
 
     @Column
     var age: Int = 0,
 
     @Column
     var content: String = ""
-
-    constructor(id: Long, name: String, age: Int) : this() {
-        this.id = id
-        this.name = name
-        this.age = age
-    }
-}
+)
